2dde80e22d9a3f0992c51d15a4b7ca84ba10a256
lirikf <43917513+lirikf@users.noreply.github.com>
2020-08-26 21:52:29 -0400

    most first attempt

---
+++ src/main/java/components/listeners/MostHandler.java
@@ -0,0 +1,19 @@
+package components.listeners;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MostHandler extends ListenerAdapter{
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        // Cancel this action if the author was the bot,
+        // or cancel if the message didn't start with our command character.
+        if (event.getAuthor().isBot() || !event.getMessage().getContentRaw().startsWith("$")) {
+            return;
+        }
+        String message = event.getMessage().getContentRaw().substring(1);
+        if (message.equals("helloBot")) {
+            event.getChannel().sendMessage("Heya Darryl!").queue();
+        }
+    }
+}
